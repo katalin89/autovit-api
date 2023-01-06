f1a8d34c1b93abb69e40432517d9bf5d380ef0c4
Kati89 <kovacs.katalin@sapientia.ms.ro>
2023-01-06 07:42:58 -1000

    first commit

---
+++ src/main/java/ro/mycode/autovitapi/AutovitApiApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.autovitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutovitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutovitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/autovitapi/AutovitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.autovitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutovitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
